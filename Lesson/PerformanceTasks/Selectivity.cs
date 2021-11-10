e37cd6c006a14b185b399192cae85c145d0178dd
trofimoff <alexzonic@yandex.ru>
2021-11-10 18:08:10 +0400

    селективность

---
+++ Lesson/PerformanceTasks/Selectivity.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace Lesson.PerformanceTasks
+{
+    public static class Selectivity
+    {
+        /*
+         * считает количество чисел от 1 до number, делящихся на 5 и 2 без остатка
+         * возвращает количетсво чисел и число делений числа
+         */
+        public static (int, int) CalculateEvenNumbers(int number)
+        {
+            var operationsCount = 0; // счетчик операций деления
+            var divisibleCount = 0;
+
+            for (var i = number; i > 0; i--)
+            {
+                if (IsDivider(i, 2, ref operationsCount) && IsDivider(i, 5, ref operationsCount))
+                {
+                    divisibleCount++;
+                }
+            }
+
+            return (divisibleCount, operationsCount);
+        }
+
+        private static bool IsDivider(int number, int divider, ref int counter)
+        {
+            ++counter;
+            return number % divider == 0;
+        }
+    }
+
+    [TestFixture]
+    public class SelectivityTests
+    {
+        [TestCase(100, 10, 120)]
+        public void Test(int number, int expectedDivisibleCount, int expectedOperationsCount)
+        {
+            var (actualDivisibleCount, actualOperationsCount) = Selectivity.CalculateEvenNumbers(number);
+
+            Assert.AreEqual(expectedDivisibleCount, actualDivisibleCount);
+            Assert.AreEqual(expectedOperationsCount, actualOperationsCount);
+        }
+    }
+}
