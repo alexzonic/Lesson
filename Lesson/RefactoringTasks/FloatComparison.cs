785a7b76b6f4818319aef99b995e6a1a4eb0df66
zhelonkin <zhelonkin.ru@gmail.com>
2021-11-11 16:06:33 +0400

    Числа с плавающей точкой

---
+++ Lesson/RefactoringTasks/FloatComparison.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Lesson.RefactoringTasks
+{
+    public static class FloatComparison
+    {
+        public static bool Do(int in1, int in2)
+        {
+            var value1 = in1 / 3d;
+            var value2 = in2 / 3d - 1;
+
+            return value1 == value2;
+        }
+    }
+
+    [TestFixture]
+    public sealed class FloatComparisonTests
+    {
+        [TestCase(0, 3, true)]
+        [TestCase(1, 4, true)]
+        [TestCase(2, 5, true)]
+        [TestCase(3, 6, true)]
+        public void ReturnAndAddTest(int in1, int in2, bool expected)
+        {
+            var actual = FloatComparison.Do(in1, in2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
